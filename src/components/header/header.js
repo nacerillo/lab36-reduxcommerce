2639650b9f062e80a7fa7dc5275774f84a500427
nacerillo <nicholascerillo@gmail.com>
2021-06-08 17:09:49 -0700

    adding ui-materail to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,39 @@
 import React from "react";
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Toolbar, Button, Link } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  button: {
+    marginRight: theme.spacing(2),
+  },
+
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 const Header = () => {
+  const classes = useStyles();
+
   return (
     <>
-      <AppBar>
-        <Toolbar>MY STORE</Toolbar>
+      <AppBar position="static">
+        <Toolbar>
+          <Button
+            edge="start"
+            className={classes.button}
+            color="inherit"
+            aria-label="menu"
+          >
+            MY STORE
+          </Button>
+          <Button className={classes.cartbutton} component={Link} to="/cart">
+            Cart
+          </Button>
+        </Toolbar>
       </AppBar>
     </>
   );
